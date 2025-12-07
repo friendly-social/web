21b3f9e3d361856e0ce062dacc29aa47bc40cdbf
Viktor Varenik <yavarenikya@gmail.com>
2025-12-07 19:55:10 +0200

    feat: impl basic client for backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+'use client';
+
+import {FriendlyClient, FriendlyClientImpl} from '@/network/friendly-client';
+import {useEffect} from 'react';
+
+const client: FriendlyClient = new FriendlyClientImpl();
+
 export default function Home() {
+    const loadData = async () => {
+        const auth = await client.generateAccount({
+            nickname: 'boykisser',
+            description: "I do like kissin' boys, btw desc.",
+            interests: ['femboys'],
+            avatar: null,
+        });
+        console.log(auth);
+    };
+
+    useEffect(() => {
+        void loadData();
+    }, []);
     return <div className="p-4 text-1xl font-bold">Hello, world!</div>;
 }
