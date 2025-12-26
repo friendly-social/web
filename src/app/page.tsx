cf093633ed4b8b36ac0dcc0394669f8afd830c15
Viktor Varenik <yavarenikya@gmail.com>
2025-12-26 12:54:39 +0200

    chore: integrate and migrate to mui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,17 @@ import {
     UserDetailsResponse,
 } from '@/network/friendly-client';
 import {useEffect, useState} from 'react';
+import {
+    Alert,
+    AppBar,
+    Avatar,
+    Button,
+    Chip,
+    IconButton,
+    Toolbar,
+    Typography,
+} from '@mui/material';
+import {CheckIcon, MenuIcon} from 'lucide-react';
 
 const client: FriendlyClient = new FriendlyClientImpl();
 
@@ -13,6 +24,8 @@ export default function Home() {
     const [userDetails, setUserDetails] = useState<UserDetailsResponse | null>(
         null,
     );
+    const [isShowAlert, setIsShowAlert] = useState(false);
+
     const loadData = async () => {
         const auth = await client.generateAccount({
             nickname: 'boykisser',
@@ -24,30 +37,59 @@ export default function Home() {
         client.setAuthToken(auth.token, auth.id.toString());
         const details = await client.getUserDetails();
         console.log(details);
+
         setUserDetails(details);
+        setIsShowAlert(true);
     };
 
     useEffect(() => {
         void loadData();
     }, []);
     return (
-        <div className="p-2 flex flex-col">
-            <div className="flex flex-col gap-2" hidden={userDetails === null}>
-                <div className="flex justify-center items-center rounded-full bg-primary w-12 h-12 text-2xl font-bold uppercase tracking-wide text-primary-foreground">
-                    {userDetails?.nickname.toString()[0]}
-                </div>
-                <p>{userDetails?.nickname}</p>
-                <p>{userDetails?.description}</p>
-                <h3>Interests:</h3>
-                {userDetails?.interests.map(interest => (
-                    <p
-                        key={interest}
-                        className="w-min px-1 py-0 rounded-3xl bg-primary text-base text-primary-foreground hover:bg-primary/70 cursor-pointer"
+        <div className="flex flex-col">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}}
                     >
-                        {interest}
-                    </p>
-                ))}
-            </div>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        Friendly
+                    </Typography>
+                    <Button color="inherit">Log out</Button>
+                </Toolbar>
+            </AppBar>
+            <Alert
+                hidden={!isShowAlert}
+                icon={<CheckIcon fontSize="inherit" />}
+                severity="success"
+                onClose={() => {
+                    setIsShowAlert(false);
+                }}
+            >
+                New account successfully registered.
+            </Alert>
+            <>
+                <div
+                    className="flex flex-col gap-2 p-2 items-center pt-8"
+                    hidden={userDetails === null}
+                >
+                    <Avatar>{userDetails?.nickname.toString()[0]}</Avatar>
+                    <p>{userDetails?.nickname}</p>
+                    <p>{userDetails?.description}</p>
+                    <h3>Interests:</h3>
+                    <div className="flex flex-row gap-2">
+                        {userDetails?.interests.map(interest => (
+                            <Chip label={interest} key={interest} />
+                        ))}
+                    </div>
+                </div>
+            </>
         </div>
     );
 }
