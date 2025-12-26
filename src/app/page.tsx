967ccc99c11dc29c4a633f7ab0175990bb446ecd
Viktor Varenik <yavarenikya@gmail.com>
2025-12-26 12:29:48 +0200

    chore: impl some base ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 'use client';
 
-import {FriendlyClient, FriendlyClientImpl} from '@/network/friendly-client';
-import {useEffect} from 'react';
+import {
+    FriendlyClient,
+    FriendlyClientImpl,
+    UserDetailsResponse,
+} from '@/network/friendly-client';
+import {useEffect, useState} from 'react';
 
 const client: FriendlyClient = new FriendlyClientImpl();
 
 export default function Home() {
+    const [userDetails, setUserDetails] = useState<UserDetailsResponse | null>(
+        null,
+    );
     const loadData = async () => {
         const auth = await client.generateAccount({
             nickname: 'boykisser',
@@ -17,10 +24,30 @@ export default function Home() {
         client.setAuthToken(auth.token, auth.id.toString());
         const details = await client.getUserDetails();
         console.log(details);
+        setUserDetails(details);
     };
 
     useEffect(() => {
         void loadData();
     }, []);
-    return <div className="p-4 text-1xl font-bold">Hello, world!</div>;
+    return (
+        <div className="p-2 flex flex-col">
+            <div className="flex flex-col gap-2" hidden={userDetails === null}>
+                <div className="flex justify-center items-center rounded-full bg-primary w-12 h-12 text-2xl font-bold uppercase tracking-wide text-primary-foreground">
+                    {userDetails?.nickname.toString()[0]}
+                </div>
+                <p>{userDetails?.nickname}</p>
+                <p>{userDetails?.description}</p>
+                <h3>Interests:</h3>
+                {userDetails?.interests.map(interest => (
+                    <p
+                        key={interest}
+                        className="w-min px-1 py-0 rounded-3xl bg-primary text-base text-primary-foreground hover:bg-primary/70 cursor-pointer"
+                    >
+                        {interest}
+                    </p>
+                ))}
+            </div>
+        </div>
+    );
 }
